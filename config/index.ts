aeaeea05eb2c62841d785740acd5131f8ca45984
ionsobol03 <ionsobol@gmail.com>
2021-01-05 17:39:00 -0500

    Changed Domain

--- config/index.ts
+++ config/index.ts
@@ -5,5 +5,5 @@ const prod = process.env.NODE_ENV === "production";
 export const server = dev
   ? "http://localhost:3000"
   : test
-  ? "https://duskbooks.vercel.app"
-  : "https://duskbooks.vercel.app";
+  ? "https://duskbooks.com"
+  : "https://duskbooks.com";
