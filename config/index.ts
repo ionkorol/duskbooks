e8d8b540efae3c38ddc0f92db14951bb2d00409a
ionsobol03 <ionsobol@gmail.com>
2021-01-05 12:38:51 -0500

    Domain Fix

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,9 @@
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV === "development";
+const test = process.env.NODE_ENV === "test";
+const prod = process.env.NODE_ENV === "production";
 
-export const server = dev ? 'http://localhost:3000' : 'https://duskbooks.vercel.app';
+export const server = dev
+  ? "http://localhost:3000"
+  : test
+  ? "https://duskbooks.vercel.app"
+  : "https://duskbooks.vercel.app";
